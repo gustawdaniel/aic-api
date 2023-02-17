2878d7bbdac6f22709869b55feec1df2d5cffdef
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-02-17 15:42:33 +0400

    validation fixed

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -6,42 +6,50 @@ import {tokenizeUser} from "../functions/auth";
 
 export class Auth {
     static async googleVerify(req: FastifyRequest<{ Body: { credential: string } }>) {
-        const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-        const ticket = await client.verifyIdToken({
-            idToken: req.body.credential,
-            audience: process.env.GOOGLE_CLIENT_ID,
-        });
-        const payload = ticket.getPayload();
-        if (!payload) throw new Error(`No payload`);
-        if(!payload.email) throw new Error(`User without email`)
+        try {
+            console.log("credential", req.body.credential);
+            const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+            console.log("GOOGLE_CLIENT_ID", process.env.GOOGLE_CLIENT_ID);
+            const ticket = await client.verifyIdToken({
+                idToken: req.body.credential,
+                audience: process.env.GOOGLE_CLIENT_ID,
+            });
+            const payload = ticket.getPayload();
+            console.log("payload", payload);
+            if (!payload) throw new Error(`No payload`);
+            console.log("payload.email", payload.email);
+            if (!payload.email) throw new Error(`User without email`)
 
-        let user = await prisma.users.findUnique({
-            where: {
-                email: payload.email
-            }
-        })
-
-        if(!user) {
-            user = await prisma.users.create({
-                data: {
-                    email: payload.email,
-                    avatar: payload.picture ?? `https://ui-avatars.com/api/?name=${payload.name}`,
-                    full_name: payload.name ?? ''
+            let user = await prisma.users.findUnique({
+                where: {
+                    email: payload.email
                 }
             })
+
+            if (!user) {
+                user = await prisma.users.create({
+                    data: {
+                        email: payload.email,
+                        avatar: payload.picture ?? `https://ui-avatars.com/api/?name=${payload.name}`,
+                        full_name: payload.name ?? ''
+                    }
+                })
+            }
+
+
+            console.log(payload);
+            return {
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    avatar: user.avatar,
+                    full_name: user.full_name
+                },
+                token: tokenizeUser(user)
+            };
+        } catch (e) {
+            console.log("errr", e);
+            throw e;
         }
-
-
-
-        console.log(payload);
-        return {
-            user: {
-                id: user.id,
-                email: user.email,
-                avatar: user.avatar,
-                full_name: user.full_name
-            },
-            token: tokenizeUser(user)
-        };
     }
 }
