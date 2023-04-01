9be948b8f60cbbcd187110cc37f1ce9f7fd44b83
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-04-01 15:18:56 +0800

    added migration

---
+++ migrations/1680192430746_Migration.ts
@@ -0,0 +1,42 @@
+import { Db } from 'mongodb'
+import { MigrationInterface } from 'mongo-migrate-ts';
+import { getArticleTitle } from "../src/functions/getArticleTitle";
+
+export class Migration1680192430746 implements MigrationInterface {
+  public async up(db: Db): Promise<any> {
+    await db.collection('requests').updateMany({}, {
+      $unset: {
+        html: 1
+      }
+    });
+    const articles = await db.collection('articles').find().toArray();
+    for (const article of articles) {
+      const request = await db.collection('requests').findOne({
+        _id: article.request_id
+      });
+      await db.collection('articles').updateOne({
+        _id: article._id
+      }, {
+        $set: {
+          title: getArticleTitle({components: article.components}),
+          source_url: request ? request.url : ''
+        }
+      })
+    }
+  }
+
+  public async down(db: Db): Promise<any> {
+    await db.collection('requests').updateMany({}, {
+      $set: {
+        html: ''
+      }
+    });
+    await db.collection('articles').updateMany({}, {
+      $unset: {
+        title: '',
+        source_url: ''
+      }
+    });
+
+  }
+}
